bbaf13749028304c8c0fb92816c592a46167c234
unknown <stretchegan@gmail.com>
2026-05-25 15:20:54 +0000

    Added person movie credits navigation

--- src/app/pages/person-details/person-details.page.ts
+++ src/app/pages/person-details/person-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-person-details',
@@ -18,10 +19,14 @@ export class PersonDetailsPage implements OnInit {
 
   person: any;
   personDetails: any;
+  movies: any[] = [];
 
   apiKey = 'aec8cb7579247fdf0ce50a43711f7308';
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {
     const nav = history.state;
     this.person = nav.person;
   }
@@ -29,6 +34,7 @@ export class PersonDetailsPage implements OnInit {
   ngOnInit() {
     if (this.person) {
       this.loadPersonDetails();
+      this.loadMovies();
     }
   }
 
@@ -41,11 +47,34 @@ export class PersonDetailsPage implements OnInit {
     });
   }
 
+  loadMovies() {
+    const url =
+      `https://api.themoviedb.org/3/person/${this.person.id}/movie_credits?api_key=${this.apiKey}`;
+
+    this.http.get<any>(url).subscribe(response => {
+      this.movies = response.cast;
+    });
+  }
+
   getProfileUrl(path: string) {
     if (!path) {
       return '';
     }
 
     return `https://image.tmdb.org/t/p/w300${path}`;
+  }
+
+  getPosterUrl(path: string) {
+    if (!path) {
+      return '';
+    }
+
+    return `https://image.tmdb.org/t/p/w300${path}`;
+  }
+
+  openMovie(movie: any) {
+    this.router.navigate(['/movie-details'], {
+      state: { movie: movie }
+    });
   }
 }
